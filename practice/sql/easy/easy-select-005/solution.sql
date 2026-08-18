fd34fa1bce3fdff5ee3afcd0323bff62148d9fdc
BuiHang-stp <236509182+BuiHang-stp@users.noreply.github.com>
2026-08-18 10:12:11 +0000

    solve: 5 problems (2026-08-18)
    - easy-select-001
    - easy-where-010
    - easy-select-005
    - easy-sum-003
    - py-average

---
+++ practice/sql/easy/easy-select-005/solution.sql
@@ -0,0 +1,10 @@
+-- Xom Data · Store list by city
+-- Problem: https://xomdata.com/practice/easy-select-005
+-- Solved: 2026-08-18
+
+SELECT 
+    name,
+    city
+FROM stores
+ORDER BY city ASC,
+         name ASC;
